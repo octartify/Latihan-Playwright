d7d2bdba987920f168b72da2cb42eb74aa2125bd
octartify <octaviaplarasati@gmail.com>
2025-06-24 14:31:01 +0700

    update

--- test-pertama.spec.js
+++ test-pertama.spec.js
@@ -1,8 +1,9 @@
 const { test, expect } = require('@playwright/test');
 //
-test('isi form di demoqa', async ({ page }) => {
+test('Fill form in Text-box menu', async ({ page }) => {
   await page.goto('https://demoqa.com/text-box');
-  
+  await page.waitForSelector('#userName');
+
   await page.fill('#userName', 'Octavia');
   await page.fill('#userEmail', 'test@gmail.com');
   await page.fill('#currentAddress', 'Yogya');
@@ -11,10 +12,19 @@ test('isi form di demoqa', async ({ page }) => {
   await page.screenshot({ path: 'hasil-demoqa-form.png' });
 });
 
-test('checklist checkbox di demoqa', async ({ page }) => {
+test('Expand-Collapse button in Checkbox menu', async ({ page }) => {
   await page.goto('https://demoqa.com/checkbox');
   
   await page.click('button[Aria-label="Expand all"]');
   await page.screenshot({ path: 'expand-all.png' });
+  await page.click('button[Aria-label="Collapse all"]');
+  await page.screenshot({ path: 'collapse-all.png' });
+  await page.locator('li:has(label[for="tree-node-home"]) >> button.rct-collapse').click();
 });
 
+test('Chevron function in Checkbox menu', async ({ page }) => {
+   await page.goto('https://demoqa.com/checkbox');
+   await page.waitForSelector('button[aria-label="Toggle"]');
+
+   await page.locator('li:has(label[for="tree-node-home"]) >> button.rct-collapse').click();
+ });
